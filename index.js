5d367d1a2818e813853b3b01554c559bda1fcd57
54145a <133004095+54145a@users.noreply.github.com>
2024-07-16 12:23:08 +0000

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import fs from "node:fs";
 
-async function fetchAndWriteFile(file, url){
-    return await fetch(url)
-    .then(v=>v.text())
-    .then(v=>fs.writeFileSync(file, v));
+async function fetchAndWriteFile(file, url) {
+  
 }
+
+async function archivePost(id) {
+  if (!fs.existsSync("output")) {
+    fs.mkdirSync("output");
+  }
+  const time = Date.now();
+  if (!fs.existsSync(`output/${time}`)) {
+    fs.mkdirSync(`output/${time}`);
+  }
+  fs.mkdirSync(`output/${time}/${id}`);
+  const detailsText = await (await fetch("https://api.codemao.cn/web/forums/posts/${id}/details")).text();
+  fs.writeFileSync(`output/${time}/${id}/post.json`,detailsText);
+  const n_replies = JSON.parse(detailsText).n_replies;
+  const repliesIndex = await (await fetch("https://api.codemao.cn/web/")).text();
+}
+
+archivePost("682227");
