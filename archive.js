e1e7e165584193fffdb6bd9eba35c9cb5f639608
145a <133004095+54145a@users.noreply.github.com>
2024-08-30 09:02:50 +0000

    test

--- archive.js
+++ archive.js
@@ -3,16 +3,16 @@ import fs from "node:fs";
 
 async function archivePost(id, timestamp) {
   const details = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/details`)).json();
-  if(details.error_code){
-    console.log(id, details.error_code);
-    return false;
+  if (details.error_code) {
+    console.log(id, details.error_message);
+    return details.error_code.startsWith("Limit-Operation") ? await archivePost(id, timestamp) : false;
   }
-  console.log(id);
   fs.mkdirSync(`output/${timestamp}/${id}`);
   fs.writeFileSync(`output/${timestamp}/${id}/post.json`, JSON.stringify(details));
   const n_replies = details.n_replies;
   const repliesText = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/replies?page=1&limit=30&sort=-n_likes`)).json();
   fs.writeFileSync(`output/${timestamp}/${id}/replies.json`, JSON.stringify(repliesText));
+  console.log(id, "存档完成");
 }
 async function archive(postIdList) {
   if (!fs.existsSync("output")) {
