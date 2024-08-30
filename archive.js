37db6e822557c8e65475bb7176a2f6dadedcf013
145a <133004095+54145a@users.noreply.github.com>
2024-08-30 07:53:37 +0000

    test

--- archive.js
+++ archive.js
@@ -1,20 +1,27 @@
+import console from "node:console";
 import fs from "node:fs";
 
-
-
 async function archivePost(id, timestamp) {
+  const details = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/details`)).json();
+  if(details.error_code){
+    console.log(id, details.error_code);
+    return false;
+  }
+  console.log(id);
+  fs.mkdirSync(`output/${timestamp}/${id}`);
+  fs.writeFileSync(`output/${timestamp}/${id}/post.json`, JSON.stringify(details));
+  const n_replies = details.n_replies;
+  const repliesText = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/replies?page=1&limit=30&sort=-n_likes`)).json();
+  fs.writeFileSync(`output/${timestamp}/${id}/replies.json`, JSON.stringify(repliesText));
+}
+async function archive(postIdList) {
   if (!fs.existsSync("output")) {
     fs.mkdirSync("output");
   }
-  if (!fs.existsSync(`output/${timestamp}`)) {
-    fs.mkdirSync(`output/${timestamp}`);
+  const timestamp = Date.now();
+  fs.mkdirSync(`output/${timestamp}`);
+  for (const id of postIdList) {
+    await archivePost(id, timestamp);
   }
-  fs.mkdirSync(`output/${timestamp}/${id}`);
-  const detailsText = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/details`)).text();
-  fs.writeFileSync(`output/${timestamp}/${id}/post.json`, detailsText);
-  const n_replies = JSON.parse(detailsText).n_replies;
-  const repliesText = await (await fetch(`https://api.codemao.cn/web/forums/posts/${id}/replies?page=1&limit=30&sort=-n_likes`)).text();
-  fs.writeFileSync(`output/${timestamp}/${id}/replies.json`, repliesText);
 }
-
-export { archivePost };
+export { archive };
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-import { archivePost } from "./archive.js";
+import fs from "node:fs";
+import { archive } from "./archive.js";
+//const archivedPostIdList = fs.readdirSync(`output/${Math.max(...fs.readdirSync("output").map(v => +v))}`);
+archive(Array.from({length: 1000000}, (v, i) => i + 1));
