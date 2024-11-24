289414934012f965dfe60430e8204bb32b89b4b4
145a <133004095+54145a@users.noreply.github.com>
2024-11-24 03:56:55 +0000

    16088

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import fs from "node:fs";
 import { archive } from "./archive.js";
 const archivedPostIdList = fs.readdirSync("output").map(i => +i);
 const lastArchivedPostId = Math.max(...archivedPostIdList);
-archive(...archivedPostIdList, Array.from(Array(100000)).map((v, i) => i + 1 + lastArchivedPostId));
+archive(Array.from(Array(100000)).map((v, i) => i + 1 + lastArchivedPostId));
